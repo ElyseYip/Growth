07c0b88ef6e1168730e11061f916783dd752014d
ElyseYip <eidiusyip@gmail.com>
2025-06-03 12:56:09 +0800

    deleted unused import

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,10 +1,7 @@
-import DateComponent from "../components/DateComponent";
-
 const MainPage: React.FC = () => {
   return (
     <>
       <div>Main</div>
-      {/* <DateComponent /> */}
     </>
   );
 };
